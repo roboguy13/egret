7e26ca9a884d4a8e5ff46a9d58177e781dcc555c
David Young <roboguy06@gmail.com>
2023-03-12 01:12:43 -0600

    Foundations for basic type checking

---
+++ src/Egret/Parser/Type.hs
@@ -0,0 +1,29 @@
+module Egret.Parser.Type
+  (parseType
+  )
+  where
+
+import           Egret.TypeChecker.Type
+
+import           Egret.Parser.Utils
+
+import           Text.Megaparsec
+
+parseType :: Parser Type
+parseType =
+  try parseFnType <|> parseType'
+
+parseType' :: Parser Type
+parseType' = lexeme $
+  try (symbol "(" *> parseType <* symbol ")")
+    <|>
+  parseBaseType
+
+parseBaseType :: Parser Type
+parseBaseType = label "base type" $ lexeme $ fmap BaseType parseIdentifier
+
+parseFnType :: Parser Type
+parseFnType = label "function type" $ lexeme $ do
+  a <- parseType'
+  symbol "->"
+  FnType a <$> parseType
---
+++ src/Egret/TypeChecker/Type.hs
@@ -0,0 +1,97 @@
+{-# LANGUAGE LambdaCase #-}
+
+module Egret.TypeChecker.Type
+  (Type (..)
+  ,TypeEnv
+  ,ensureWellTypedRewrite
+  ,typeInferEquation
+  ,typeInfer
+  ,typeCheck
+  ,isWellTyped
+  )
+  where
+
+import           Egret.Ppr
+
+import           Egret.Rewrite.Expr
+import           Egret.Rewrite.Rewrite
+import           Egret.Rewrite.Equation
+
+import           Control.Monad.Reader
+
+import           Control.Applicative
+
+data Type
+  = BaseType String
+  | FnType Type Type
+  deriving (Show, Eq)
+
+instance Ppr Type where
+  pprDoc (BaseType str) = text str
+  pprDoc (FnType a b) = sep [pprDoc a, text "->", pprDoc b]
+
+-- | Well-typed input ==> well-typed output
+ensureWellTypedRewrite :: TypeEnv -> Rewrite Expr String -> Rewrite Expr String
+ensureWellTypedRewrite env re = Rewrite $ \x ->
+  let r = runRewrite re x
+  in
+  if not (isWellTyped env x) || or (fmap (isWellTyped env) r)
+    then r
+    else Nothing
+
+type TypeEnv = [(String, Type)]
+
+type TypeCheck = ReaderT TypeEnv (Either String)
+
+typeInferEquation :: TypeEnv -> Equation Expr String -> Either String Type
+typeInferEquation env (lhs0 :=: rhs0) =
+  go (lhs0 :=: rhs0) <|> go (rhs0 :=: lhs0)
+  where
+    go (lhs :=: rhs) =
+      typeCheck env rhs =<< typeInfer env lhs
+
+errorExpected :: String -> String -> Either String a
+errorExpected expected found =
+  Left $ "Expected " ++ expected ++ ", found " ++ found
+
+isWellTyped :: TypeEnv -> Expr String -> Bool
+isWellTyped env (V a) = True
+isWellTyped env e =
+  case typeInfer env e of
+    Left {} -> False
+    Right {} -> True
+
+typeInfer :: TypeEnv -> Expr String -> Either String Type
+typeInfer env e = runReaderT (typeInfer' e) env
+
+typeCheck :: TypeEnv -> Expr String -> Type -> Either String Type
+typeCheck env e ty = runReaderT (typeCheck' e ty) env
+
+typeInfer' :: Expr String -> TypeCheck Type
+typeInfer' (V a) =
+  asks (lookup a) >>= \case
+    Nothing -> lift $ Left $ "typeInfer': Cannot find variable " ++ a
+    Just ty -> pure ty
+typeInfer' (App f x) = do
+  typeInfer' f >>= \case
+    FnType a b -> do
+      typeCheck' x a
+      pure b
+    fTy -> lift $ errorExpected "function type" (ppr fTy)
+
+typeCheck' :: Expr String -> Type -> TypeCheck Type
+typeCheck' (V a) ty =
+  asks (lookup a) >>= \case
+    Nothing -> pure ty
+    Just ty'
+      | ty' == ty -> pure ty
+      | otherwise -> lift $ errorExpected (ppr ty) (ppr ty')
+
+typeCheck' (App f x) ty =
+  case ty of
+    FnType a b -> do
+      typeCheck' f a
+      typeCheck' x b
+      pure ty
+    _ -> lift $ errorExpected "function type" (ppr ty)
+
